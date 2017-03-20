6985a3974baf277e2740d91505e1a7fa39ce4fbc
voidbluelabtop <kimqwe12300@daum.net>
2017-03-20 09:41:08 +0900

    RNN Test

---
+++ ML_KJY/pylib/RNN.py
@@ -0,0 +1,100 @@
+import tensorflow as tf
+import numpy as np
+
+
+sess = tf.Session()
+
+char_rdic = ['h', 'e', 'l', 'o', 'w', 'r', 'd'] # id -> char
+char_dic = {w : i for i, w in enumerate(char_rdic)} # char -> id
+print (char_dic)
+
+
+
+ground_truth = [char_dic[c] for c in 'hellowor']
+print (ground_truth)
+
+x_data = np.array([[1,0,0,0,0,0,0], # h
+                   [0,1,0,0,0,0,0], # e
+                   [0,0,1,0,0,0,0], # l
+                   [0,0,0,1,0,0,0], # o
+                     [0, 0, 0, 0, 1, 0, 0], # w
+                     [0, 0, 0, 0, 0, 1, 0], # r
+                     [0, 0, 0, 0, 0, 0, 1]],dtype= np.float32) # d
+
+
+# Configuration
+rnn_size = len(char_dic) # 7
+batch_size = 1
+output_size = 7
+
+
+
+# RNN Model
+rnn_cell = tf.contrib.rnn.BasicRNNCell(num_units = rnn_size,
+                                       input_size = None, # deprecated at tensorflow 0.9
+                                       #activation = tanh,
+                                       )
+
+print(rnn_cell)
+
+initial_state = rnn_cell.zero_state(batch_size, tf.float32)
+print(initial_state)
+
+
+
+initial_state_1 = tf.zeros([batch_size, rnn_cell.state_size]) #  위 코드와 같은 결과
+print(initial_state_1)
+
+
+print (x_data)
+x_split = tf.split(x_data, rnn_size, 0) # 가로축으로 4개로 split
+#
+# sess.run(x_split)
+# sess.run(x_split[0])
+print(x_split)
+
+#outputs, state = tf.nn.rnn(cell = rnn_cell, inputs = x_split, initial_state = initial_state)   #구버전
+outputs, state = tf.contrib.rnn.static_rnn(cell = rnn_cell, inputs = x_split, initial_state = initial_state)
+
+print (outputs)
+print (state)
+
+logits = tf.reshape(tf.concat(outputs,1), # shape = 1 x 16 버전업으로 파라미터 순서바뀜
+                    [-1,rnn_size])        # shape = 4 x 4
+logits.get_shape()
+
+targets = tf.reshape(ground_truth[1:], [-1]) # a shape of [-1] flattens into 1-D
+targets.get_shape()
+
+
+sess.run(tf.initialize_all_variables())
+
+weights = tf.ones([len(char_dic) * batch_size])
+# seq2seq.sequence_loss의 오류검출 코드에서 리스트는 안되고 꼭 텐서로 해야 된다고 합니다.
+# 하지만 2,1,1차원 변수를 함수가 요구하는 3,2,2차원 변수로 만들기 위해 이렇게 조정해줍니다.
+d3_logics = tf.Variable([logits])
+d2_targets = tf.Variable([targets])
+d2_weights = tf.Variable([weights])
+#loss = tf.nn.seq2seq.sequence_loss_by_example([logits], [targets], [weights]) <<원랜 이게 됐었다고합니다.
+loss = tf.contrib.seq2seq.sequence_loss(d3_logics, d2_targets, d2_weights)
+cost = tf.reduce_sum(loss) / batch_size
+train_op = tf.train.RMSPropOptimizer(0.01, 0.9).minimize(cost)
+
+# Launch the graph in a session
+sess.run(tf.initialize_all_variables())
+for i in range(300):
+    sess.run(train_op)
+    result = sess.run(tf.argmax(d3_logics[0], 1))
+    if i% 20 == 0:
+        print(sess.run(cost))
+        print(result, [char_rdic[t] for t in result])
+print(sess.run(d2_weights))
+
+
+def predic(chr):
+    xxx = char_rdic.index(chr)
+    returnx = sess.run(tf.argmax([[xxx]], 1))
+    print(returnx)
+
+
+
---
+++ ML_KJY/pylib/RNN@.py
@@ -0,0 +1,38 @@
+vocabulary_size = 8000
+unknown_token = "UNKNOWN_TOKEN"
+sentence_start_token = "SENTENCE_START"
+sentence_end_token = "SENTENCE_END"
+
+# Read the data and append SENTENCE_START and SENTENCE_END tokens
+print ("Reading CSV file...")
+with open('data/reddit-comments-2015-08.csv', 'rb') as f:
+    reader = csv.reader(f, skipinitialspace=True)
+    reader.next()
+    # Split full comments into sentences
+    sentences = itertools.chain(*[nltk.sent_tokenize(x[0].decode('utf-8').lower()) for x in reader])
+    # Append SENTENCE_START and SENTENCE_END
+    sentences = ["%s %s %s" % (sentence_start_token, x, sentence_end_token) for x in sentences]
+print ("Parsed {} sentences.".format(len(sentences)))
+
+# Tokenize the sentences into words
+tokenized_sentences = [nltk.word_tokenize(sent) for sent in sentences]
+
+# Count the word frequencies
+word_freq = nltk.FreqDist(itertools.chain(*tokenized_sentences))
+print("Found {} unique words tokens.").format(len(word_freq.items()))
+
+# Get the most common words and build index_to_word and word_to_index vectors
+vocab = word_freq.most_common(vocabulary_size-1)
+index_to_word = [x[0] for x in vocab]
+index_to_word.append(unknown_token)
+word_to_index = dict([(w,i) for i,w in enumerate(index_to_word)])
+
+print ("Using vocabulary size %d.".format(vocabulary_size))
+print ("The least frequent word in our vocabulary is '{}' and appeared %d times.".format( (vocab[-1][0], vocab[-1][1])))
+
+# Replace all words not in our vocabulary with the unknown token
+for i, sent in enumerate(tokenized_sentences):
+    tokenized_sentences[i] = [w if w in word_to_index else unknown_token for w in sent]
+
+print ("\nExample sentence: '{}'".format( sentences[0]))
+print "\nExample sentence after Pre-processing: '%s'" % tokenized_sentences[0]
